efd7aa8fca53acb4f1b51c7acc1fa88f07660217
纵歌 <xw.wang@catontechnology.com>
2025-02-25 09:32:46 +0800

    add gen_rand

--- src/lib.rs
+++ src/lib.rs
@@ -3,29 +3,14 @@ use rsa::pkcs1::DecodeRsaPrivateKey;
 use rsa::pkcs1::DecodeRsaPublicKey;
 // use rsa::pkcs1::EncodeRsaPrivateKey; // 用于导出 PKCS#1 PEM 格式
 // use rsa::pkcs1::EncodeRsaPublicKey;
+use rand::distributions::Alphanumeric;
+use rand::{thread_rng, Rng};
 use rsa::{PaddingScheme, PublicKey, RsaPrivateKey, RsaPublicKey}; // 用于从 PEM 解析私钥
 use std::error::Error;
 
 // sample
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
-}
-
-// 加密函数
-pub fn encrypt_data(public_pem_str: &str, data: &str) -> Result<String, Box<dyn Error>> {
-    // 从 PEM 字符串加载公钥
-    let public_key = RsaPublicKey::from_pkcs1_pem(public_pem_str)?;
-
-    // 加密
-    let mut rng = OsRng;
-    let padding = PaddingScheme::new_pkcs1v15_encrypt();
-    let encrypted_data = public_key
-        .encrypt(&mut rng, padding, data.as_bytes())
-        .map_err(|e| Box::new(e) as Box<dyn Error>)?;
-
-    // 将加密后的数据转换为 Base64 字符串
-    let encrypted_base64 = base64::encode(&encrypted_data);
-    Ok(encrypted_base64)
 }
 
 // 解密函数
@@ -51,13 +36,27 @@ pub fn decrypt_data(
     Ok(decrypted_string)
 }
 
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
+// 加密函数
+pub fn encrypt_data(public_pem_str: &str, data: &str) -> Result<String, Box<dyn Error>> {
+    // 从 PEM 字符串加载公钥
+    let public_key = RsaPublicKey::from_pkcs1_pem(public_pem_str)?;
 
-//     #[test]
-//     fn it_works() {
-//         let result = add(2, 2);
-//         assert_eq!(result, 4);
-//     }
-// }
+    // 加密
+    let mut rng = OsRng;
+    let padding = PaddingScheme::new_pkcs1v15_encrypt();
+    let encrypted_data = public_key
+        .encrypt(&mut rng, padding, data.as_bytes())
+        .map_err(|e| Box::new(e) as Box<dyn Error>)?;
+
+    // 将加密后的数据转换为 Base64 字符串
+    let encrypted_base64 = base64::encode(&encrypted_data);
+    Ok(encrypted_base64)
+}
+
+pub fn gen_rand(len: usize) -> String {
+    let rng = thread_rng();
+    rng.sample_iter(&Alphanumeric)
+        .take(len)
+        .map(char::from)
+        .collect()
+}
